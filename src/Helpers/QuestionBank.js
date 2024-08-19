3d540cc739e7bfbcdf2bfc998f1c3adba4617af2
Orho Ben <orhorhagheese@Orhorhaghes-MacBook-Pro.local>
2024-08-19 22:57:29 +0100

    Your commit message here

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "what is 1+3?",
+        OptionA: "24",
+        OptionB: "21",
+        OptionC: "4",
+        OptionD: "2",
+        answer: "optionC"
+    },
+    {
+        prompt: "which is a streaming platform",
+        OptionA: "X",
+        OptionB: "Twitch",
+        OptionC: "Google",
+        OptionD: "Robinhod",
+        answer: "optionB"
+    },
+    {
+        prompt: "Highest number in the following number",
+        OptionA: "24",
+        OptionB: "21",
+        OptionC: "4",
+        OptionD: "2",
+        answer: "optionA"
+    }
+]
